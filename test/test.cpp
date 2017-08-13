c0fa7377e3bc5d5ac53abe07487bf5ea2fedf764
Viktor <pp_ringdahl@hotmail.com>
2017-08-13 19:48:50 +0200

    Added basic test, depends on files not included in git to function correctly.

--- test/test.cpp
+++ test/test.cpp
@@ -2,6 +2,10 @@
 
 #include "armadillo"
 #include "audioread.h"
+#include "sigprocess.h"
+#include "log.h"
+
+#define IN_TEST TRUE
 
 void passedText(bool passed) {
   if(passed)
@@ -10,18 +14,38 @@ void passedText(bool passed) {
     std::cout << "Failed\n";
 }
 
+void testSigp(Audio* a) {
+  logInf("Calc fbank");
+  arma::fmat fbank = sigp::melfilterbank(400, 20, 40, 16000, 44100, 512);
+  logInf("Calc window");
+  arma::fvec window = sigp::hamming(400);
+
+  arma::fvec d = a->getData(0, 400);
+  logInf("Calc mfcc");
+  arma::fvec mfcc = sigp::mfcc(&d, 20, &fbank, 512, &window);
+  mfcc.print();
+}
+
 // Audio tests
 void testAudioread() {
+  int status = Audio::init();
+  if (status != 0)
+    std::cout << "Error intializing\n";
   std::cout << "Testing reading of file empty.wav: ";
-  Audio a = Audio("../test/resources/empty.wav");
+  Audio a = Audio("./test/resources/empty.wav");
   arma::fvec data = a.getData(0, 99999);
   passedText(arma::accu(data % data) == 0);
 
   std::cout << "Testing reading of file notempty.wav: ";
-  a = Audio("../test/resources/notempty.wav");
-  data = a.getData(0, 99999);
+  Audio b = Audio("./test/resources/notempty.wav");
+  data = b.getData(0, 99999);
   passedText(arma::accu(data % data) != 0);
+
+  testSigp(&b);
+
+  Audio::finalize();
 }
+
 
 int main() {
   testAudioread();
